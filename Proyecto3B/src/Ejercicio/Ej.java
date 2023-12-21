a7d9ef049a7fe45fae256dc4eb6b80e6b42b1af6
Floritura <jflores8150@ceuandalucia.es>
2023-12-21 16:22:12 +0100

    Andamo ruleta

---
+++ Proyecto3B/src/Ejercicio/Ej.java
@@ -0,0 +1,11 @@
+package Ejercicio;
+
+public class Ej {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Mi nombre es Floritura");
+	}
+
+}
